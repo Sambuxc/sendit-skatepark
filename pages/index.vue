c63691f85b1f2337472fb2841101a8c6e5b6f20a
Sambuxc <samwbru@gmail.com>
2024-09-07 18:55:52 +0100

    added google maps ui

--- pages/index.vue
+++ pages/index.vue
@@ -1,16 +1,3 @@
-<script setup lang="ts">
-
-useHead({
-	title: 'Send it Stockport - Portable Skate Ramps',
-	meta: [
-		{
-			name: 'Portable skate ramps to hire, rent for birthdays and parties',
-			content: 'Portable skate ramps to hire, rent for birthdays and parties',
-		},
-	],
-});
-</script>
-
 <template>
 	<main class="w-svw h-svh">
 		<header>
@@ -20,52 +7,70 @@ useHead({
 				alt="bg image"
 			/> -->
 
-			<div class="relative flex gap-5">
+			<nav>
 				<img src="~/assets/images/send-it-skate-ramps-logo.png" alt="Send it portable skate parks" width="70" />
 
-				<!-- <h1 class="text-h5 font-weight-bold">
-					<mark class="p-2 rounded bg-white">
-						Portable skate ramps to hire, rent for birthdays and parties
-					</mark>
-				</h1> -->
-			</div>
+				<div class="nav-menu">
+					<!-- <NuxtLink to="#bookings" class="bg-blue rounded-full p-8 border-8 border-black text-black font-semibold w-40"> -->
+					<NuxtLink to="#bookings">
+						Book Us
+					</NuxtLink>
 
-			<!-- <nav class="lg:absolute lg:left-1/2 lg:-translate-x-1/2 lg:top-1/2 lg:-translate-y-1/2 flex flex-column justify-center align-center mt-4 text-center"> -->
-			<nav>
-				<!-- <NuxtLink to="#bookings" class="bg-blue rounded-full p-8 border-8 border-black text-black font-semibold w-40"> -->
-				<NuxtLink to="#bookings">
-					Book Us
-				</NuxtLink>
+					<!-- <NuxtLink to="/greet" class="bg-green mt-2 rounded-full p-8 border-8 border-black text-black font-semibold w-40"> -->
+					<NuxtLink to="#location">
+						Location
+					</NuxtLink>
+					<!-- <NuxtLink to="/greet" class="bg-red mt-2 rounded-full p-8 border-8 border-black text-black font-semibold w-40"> -->
+					<NuxtLink to="#ramps">
+						Ramps
+					</NuxtLink>
 
-				<!-- <NuxtLink to="/greet" class="bg-green mt-2 rounded-full p-8 border-8 border-black text-black font-semibold w-40"> -->
-				<NuxtLink to="/greet">
-					Location
-				</NuxtLink>
-				<!-- <NuxtLink to="/greet" class="bg-red mt-2 rounded-full p-8 border-8 border-black text-black font-semibold w-40"> -->
-				<NuxtLink to="/greet">
-					Ramps
-				</NuxtLink>
-
-				<!-- <NuxtLink to="/greet" class="bg-yellow mt-2 rounded-full p-8 border-8 border-black text-black font-semibold w-40"> -->
-				<NuxtLink to="/greet">
-					Contact
-				</NuxtLink>
+					<!-- <NuxtLink to="/greet" class="bg-yellow mt-2 rounded-full p-8 border-8 border-black text-black font-semibold w-40"> -->
+					<NuxtLink to="#contact">
+						Contact
+					</NuxtLink>
+				</div>
 			</nav>
 
-			<a href="#bookings" class="absolute bottom-0 left-1/2 m-3 rotate-180 text-white font-bold text-6xl hover:cursor-pointer">^</a>
+			<a href="#bookings" class="absolute bottom-0 left-1/2 m-3 font-bold text-6xl hover:cursor-pointer">↓</a>
 		</header>
 
-		<section id="bookings" class="h-svh mt-5 p-4">
+		<section id="bookings" class="p-5 bg-blue">
 			<h2>
-				<mark class="bg-blue"> Book your next skate </mark>
+				<mark class="bg-yellow"> Book your next skate </mark>
 			</h2>
 
-			<iframe class="mt-5 lg:w-1/2 mx-auto"
+			<iframe class="mt-5 w-full lg:w-1/2 mx-auto"
 				src="https://docs.google.com/forms/d/e/1FAIpQLSclgSffLthp4IqrC4rBlOgosUPEsH57W8Gh92Qx-VhrEuqE-A/viewform?embedded=true"
 				width="640" height="1659" frameborder="0" marginheight="0" marginwidth="0">Loading…</iframe>
 		</section>
+
+		<section id="location" class="p-5 bg-green">
+			<h2>Find Us In Your Local Area</h2>
+
+			<GMap
+				class="bg-transparent"
+				api-key="AIzaSyDWEqkzNgzXVCRUuzd1c4PPdjlfqy21KLA"
+				:location="{lat: 53.406361973195494, lng: -2.1537673826365156}"
+				:map-style-option="{ borderRadius: '9px', border: '4px solid orange' }"
+			/>
+		</section>
 	</main>
 </template>
+
+<script lang="ts" setup>
+import GMap from '~/components/GoogleMap.vue'
+
+useHead({
+		title: 'Send it Stockport - Portable Skate Ramps',
+		meta: [
+			{
+				name: 'Portable skate ramps to hire, rent for birthdays and parties',
+				content: 'Portable skate ramps to hire, rent for birthdays and parties',
+			},
+		],
+})
+</script>
 
 <style lang="scss">
 header {
@@ -79,7 +84,11 @@ header {
 }
 
 nav {
-	@apply relative flex flex-col lg:flex-row justify-center align-middle gap-5 text-center;
+	@apply relative flex flex-col lg:flex-row justify-between gap-5 text-center w-full;
+
+	.nav-menu {
+		@apply flex flex-col lg:flex-row gap-5 items-center lg:items-start justify-center;
+	}
 
 	a {
 		@apply hover:bg-blue-100 rounded-full p-5 border-4 border-black text-base text-black font-semibold;
