78cad703fa736f64054aa58cda8e72546371e891
Samuel Bruton <samuel.bruton@ca.vu>
2025-04-22 09:55:03 +0100

    - adds new eventspage with calendar style - moves default header into component

---
+++ README.md
@@ -0,0 +1,5 @@
+To publish this app, run `nuxt build` and `nuxt generate`.
+
+The generated files will be in the `dist` directory.
+
+Then commit these files to release branch.
--- app.vue
+++ app.vue
@@ -1,8 +1,5 @@
 <script lang="ts" setup>
 const Route = useRoute();
-const {
-	public: { baseUrl },
-} = useRuntimeConfig();
 </script>
 
 <template>
@@ -10,13 +7,5 @@ const {
 		class="v-theme--baseTheme d-flex flex-column justify-center align-center"
 	>
 		<NuxtPage />
-
-		<NuxtLink
-			v-if="Route.path !== baseUrl"
-			:to="baseUrl"
-			class="text-h6 text-primary mt-3"
-		>
-			Return Home
-		</NuxtLink>
 	</div>
 </template>
